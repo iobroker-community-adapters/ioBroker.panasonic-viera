2122c237752fc959565300382fe9b4993221d4d2
Ingo Fischer <github@fischer-ka.de>
2018-01-02 13:37:24 +0100

    fix util.js usage and use utils.Adapter

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
 // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
-var adapter = utils.adapter('panasonic-viera');
+var adapter = utils.Adapter('panasonic-viera');
 
 var device;
 var isConnected = null;
