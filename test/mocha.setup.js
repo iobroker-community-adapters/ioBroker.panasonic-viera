cc99d8087d4097ae35df66717720dd816e3157c4
mcm1957 <mcm57@gmx.at>
2023-05-02 22:24:53 +0200

    update tests

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -1,13 +1,13 @@
 // Don't silently swallow unhandled rejections
-process.on('unhandledRejection', error => {
-  throw error;
+process.on('unhandledRejection', (e) => {
+    throw e;
 });
 
-// Enable the should interface with sinon
+// enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
 const sinonChai = require('sinon-chai');
 const chaiAsPromised = require('chai-as-promised');
-const {should, use} = require('chai');
+const { should, use } = require('chai');
 
 should();
 use(sinonChai);
